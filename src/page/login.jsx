d4ca883da5b2a38b37c33beb72994f3e00d775db
Subha12125 <subhabratapaul12125@gmail.com>
2026-01-15 10:25:30 +0530

    registration and login page done

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -6,51 +6,58 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import { FaLock } from "react-icons/fa";
+import axios from "axios";
 
 export const Login = () => {
     const navigate = useNavigate();
-
+    const API_URL = process.env.REACT_APP_API_URL;
     const [form, setForm] = useState({
         email: "",
         password: "",
 
     });
 
-   
-    const handleChange = (e) => {
+
+    const handleChange =  (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        console.log("Login Payload : ", {
-            email: form.email,
-            password: form.password
-        });
+    const handleLogin = async (e) => {
+  e.preventDefault();
 
-        toast.success("Login Successfull");
-        setForm({
-            email:"",
-            password: ""
-        });
-
-        // const passwordRegex = /^(?=.*[!@#$%^&*]).{8,}$/;
-        // if (!passwordRegex.test(form.password)) {
-        //     toast.error("Inavlid Pass");
-        //     return;
-        // }
-
-        // showToast("Registration Successful!");
-
-        setTimeout(() => {
-            navigate("/"); // home page
-        }, 1500);
+  try {
+    const payload = {
+      email: form.email,
+      password: form.password,
     };
+
+    console.log("Sending to backend from Login page", payload);
+
+    await axios.post(`${API_URL}/login`, payload);
+
+    toast.success("Login successful!");
+
+    setTimeout(() => {
+      navigate("/");
+    }, 1500);
+
+    setForm({ email: "", password: "" });
+
+  } catch (error) {
+    console.log("Full error:", error);
+
+    if (error.response) {
+      toast.error(error.response.data.msg || "Invalid credentials");
+    } else {
+      toast.error("Server not responding");
+    }
+  }
+};
 
     return (
         <>
             <ToastContainer position='top-right' autoClose={2000} hideProgressBar={true}
-            closeOnClick pauseOnHover theme='dark'
+                closeOnClick pauseOnHover theme='dark'
             />
             <div className="login-container">
                 <form className="login-card" onSubmit={handleLogin}>
@@ -58,12 +65,12 @@ export const Login = () => {
 
                     <div className="form-group">
                         <label>Email: </label>
-                        <input type="email" name="email" required onChange={handleChange} value={form.email}/>
+                        <input type="email" name="email" required onChange={handleChange} value={form.email} />
                     </div>
 
                     <div className="form-group">
-                        <label><FaLock className='input-icon'/>Password: </label>
-                        <input type="password" name="password" required onChange={handleChange} value={form.password}/>
+                        <label><FaLock className='input-icon' />Password: </label>
+                        <input type="password" name="password" required onChange={handleChange} value={form.password} />
                     </div>
 
                     <button className="login-btn">Login</button>
